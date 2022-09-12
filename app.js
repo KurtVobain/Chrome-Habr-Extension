5c4dc96fb0bd00fff9d40472cf43dcb37f328bb3
evgenii.karpov <one1two215@yandex.ru>
2022-09-12 18:16:59 +0300

    Working extension

---
+++ app.js
@@ -0,0 +1,8 @@
+const sidebar = document.getElementsByClassName("tm-page__sidebar");
+
+
+if (sidebar) {
+  // If sider is here
+  // add attribute display: None
+  sidebar.item(0).setAttribute("style", "display:none;")
+}
